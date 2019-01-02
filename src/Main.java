325969233ae0499db5b51045bd8d273bcd34093b
willy7728 <107021050@gm.asia.edu.tw>
2019-01-02 13:34:59 +0800

    start

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn =new Scanner(System.in);
+//        int n=scn.nextInt(),max=Integer.MIN_VALUE;
+//        int[] data=new int[n];
+//        for (int i=0;i<n;i++){
+//            data[i]=scn.nextInt();
+//        }
+//        for (int i=0;i<n;i++){
+//           int sum=1;
+//            for (int j=i;j<n;j++){
+//                sum=sum*data[j];
+//                if (sum>max){
+//                    max=sum;
+//                }
+//
+//            }
+//        }
+//        System.out.println(max);
+        //02
+
+    }
+}
